a01accd1979a2aed9209fe196a378ad2142c68aa
Xu Song Lin <11956-xu_song_lin@users.noreply.sgts.gitlab-dedicated.com>
2024-10-16 14:52:16 +0800

    Song 16/10 - added ratelimiter filter

--- APPS/gateway/src/main/java/com/gateway/GatewayApplication.java
+++ APPS/gateway/src/main/java/com/gateway/GatewayApplication.java
@@ -2,25 +2,11 @@ package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator routerBuilder(RouteLocatorBuilder routeLocatorBuilder){
-		return routeLocatorBuilder.routes()
-				.route("tutget-main",
-						r -> r.path("/**")
-								.filters(f -> f.circuitBreaker(c -> c.setName("tutget-main").setFallbackUri("forward:/http://localhost:8080")))
-								.uri("lb://tutget-main")
-				)
-				.build();
 	}
 }
