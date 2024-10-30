9ba31dcda3ae525bfade992fff0bcaf94a2adcad
Xu Song Lin <11956-xu_song_lin@users.noreply.sgts.gitlab-dedicated.com>
2024-10-30 14:15:02 +0800

    Song 30/10: - fixes

--- APPS/tutget-main/src/main/java/com/tutget/tutgetmain/SecurityConfiguration.java
+++ APPS/tutget-main/src/main/java/com/tutget/tutgetmain/SecurityConfiguration.java
@@ -8,9 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
@@ -26,7 +30,7 @@ import java.io.IOException;
 import java.util.function.Supplier;
 
 @Configuration
-//@EnableFluxSecurity
+//@EnableWebSecurity
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
