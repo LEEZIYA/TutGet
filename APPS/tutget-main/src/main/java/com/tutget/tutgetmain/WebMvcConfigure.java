6c6292a7265f9c6ca9d563e80920372919535769
Xu Song Lin <11956-xu_song_lin@users.noreply.sgts.gitlab-dedicated.com>
2024-10-31 16:12:45 +0800

    Song 31/10: - merge fixes

--- APPS/tutget-main/src/main/java/com/tutget/tutgetmain/WebMvcConfigure.java
+++ APPS/tutget-main/src/main/java/com/tutget/tutgetmain/WebMvcConfigure.java
@@ -3,6 +3,7 @@ package com.tutget.tutgetmain;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
